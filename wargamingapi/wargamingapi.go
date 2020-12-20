d1dc325aea13d5374a83accdb2e79131da86200b
Cufee <vkouzin@outlook.com>
2020-12-20 06:22:46 -0800

    switched WG API calls to https

--- wargamingapi/wargamingapi.go
+++ wargamingapi/wargamingapi.go
@@ -40,16 +40,16 @@ func getJSON(url string, target interface{}) error {
 func getAPIDomain(realm string) (string, error) {
 	realm = strings.ToUpper(realm)
 	if realm == "NA" {
-		return "http://api.wotblitz.com", nil
+		return "https://api.wotblitz.com", nil
 
 	} else if realm == "EU" {
-		return "http://api.wotblitz.eu", nil
+		return "https://api.wotblitz.eu", nil
 
 	} else if realm == "RU" {
-		return "http://api.wotblitz.ru", nil
+		return "https://api.wotblitz.ru", nil
 
 	} else if realm == "ASIA" || realm == "AS" {
-		return "http://api.wotblitz.asia", nil
+		return "https://api.wotblitz.asia", nil
 
 	} else {
 		message := fmt.Sprintf("Realm %s not found", realm)
