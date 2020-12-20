930326f9f291ca49d9686e4f2ab31c7d482d651e
Cufee <vkouzin@outlook.com>
2020-12-20 07:36:55 -0800

    added restart unless stopeed

--- full_build.sh
+++ full_build.sh
@@ -8,4 +8,4 @@ export PATH=$PATH:/usr/local/go/bin
 go build -o build/app
 
 sudo docker build -t cufee/am-stats .
-sudo docker run -d --name am-stats -p 6969:4000 cufee/am-stats:latest
+sudo docker run -d --name --restart unless-stopped am-stats -p 6969:4000 cufee/am-stats:latest
