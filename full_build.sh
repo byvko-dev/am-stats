77941a4347258a0dcae3dc6a828df0ff9b0219e4
Vovko <vkouzin@outlook.com>
2020-12-21 22:38:40 -0500

    edited build script

--- full_build.sh
+++ full_build.sh
@@ -1,11 +1,10 @@
 clear
 git pull
+export PATH=$PATH:/usr/local/go/bin
+go build -o build/app
 
 sudo docker stop am-stats
 sudo docker rm am-stats
-
-export PATH=$PATH:/usr/local/go/bin
-go build -o build/app
 
 sudo docker build -t cufee/am-stats .
 sudo docker run -d --name --restart unless-stopped am-stats -p 6969:4000 cufee/am-stats:latest
